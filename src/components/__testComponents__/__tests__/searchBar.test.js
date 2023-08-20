bec0cf0ff37d801bfee37261a86d04f7bc731a48
Carol Wargo <Carolwargo@gmail.com>
2023-08-20 15:31:57 -0400

    update testing suite

---
+++ src/components/__testComponents__/__tests__/searchBar.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import pretty from 'pretty';
+import SearchBar from '../searchBar.js';
+
+let container = null;
+
+beforeEach(() => {
+  // Setup a DOM element as the target
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // Cleanup on exiting to prevent this test from altering the results of future tests
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+describe('SearchBar', () => {
+  // In this example, we render the SearchBar component and format the rendered HTML using the pretty package before saving it as a snapshot
+  it('should render and match snapshot', () => {
+    // eslint-disable-next-line testing-library/no-unnecessary-act
+    act(() => {
+      // Render the component
+      render(<SearchBar />, container);
+    });
+
+    // Format the rendered output
+    const html = pretty(container.innerHTML);
+
+    // Verify the output
+    expect(html).toMatchSnapshot();
+  });
+});
