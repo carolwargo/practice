14529699d09a7a8133e8ad1b6868c96e4f31a834
Carol Wargo <Carolwargo@gmail.com>
2023-08-20 14:36:33 -0400

    update

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Thermostat from './components/Thermostat';
 import OmdbContainer from './components/Search/OmdbContainer';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
+import Section from './components/Section/Section';
 
 // This function is a functional component that helps us split the UI into distinct parts.
 // In this case, we are returning another component, <HelloReact/> from it.
@@ -27,6 +28,7 @@ export default function App() {
             <OmdbContainer/>
             <Header />
             <Navbar />
+            <Section />
         </React.StrictMode>
     );
 }
