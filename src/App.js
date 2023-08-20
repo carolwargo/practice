e01a15c2a7f6dc1977a2c2a08db834b310cae0ee
Carol Wargo <Carolwargo@gmail.com>
2023-08-20 10:36:01 -0400

    added hello component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import './App.css';
-import HomePage from './views/HomePage/index.js';
-import Display from './components/Display.js';
+import React from 'react';
+import Hello from './components/Hello';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       <HomePage/>
-       <Display/>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <React.StrictMode>
+            <Hello />
+        </React.StrictMode>
+    );
 }
-
-export default App;
---
+++ src/components/Hello.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Hello() {
+    const message = "Hello World";
+    return (
+        <div className="container">
+            <h1>{message}</h1>
+        </div>
+    );
+}
