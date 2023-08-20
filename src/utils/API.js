f871abf85c105a03da418e2588970d658cefe429
Carol Wargo <Carolwargo@gmail.com>
2023-08-20 14:13:48 -0400

    fixed API.js error

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,4 +3,9 @@ import axios from 'axios';
 const search = async (query) =>
   axios.get(`http://www.omdbapi.com/?apikey=trilogy&t=${query}&rating=pg`);
 
-export default { search };
+const api = {
+  search
+};
+
+export default api;
+
