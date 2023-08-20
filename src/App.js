057c8057b6a5c4fc5836bcd536484312cad65a02
Carol Wargo <Carolwargo@gmail.com>
2023-08-20 14:19:03 -0400

    organizing file structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Hello from './components/Hello';
 import JSXVariables from './components/JSXVariables';
-import Display from './components/Display';
-import List from './components/List';
+import Display from './components/List/Display';
+import List from './components/List/List';
 import Greeting from './components/Greeting';
 import Counter from './components/Counter';
 import Form from './components/Form/index.js';
 import Thermostat from './components/Thermostat';
 import OmdbContainer from './components/Search/OmdbContainer';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
 
 // This function is a functional component that helps us split the UI into distinct parts.
 // In this case, we are returning another component, <HelloReact/> from it.
@@ -23,6 +25,8 @@ export default function App() {
             <Form />
             <Thermostat />
             <OmdbContainer/>
+            <Header />
+            <Navbar />
         </React.StrictMode>
     );
 }
